ce347e0e1ff9230564cada108ff2f2f2e3920809
João Fernandes <joaofnds@joaofnds.com>
2020-09-21 09:08:57 -0300

    Use datomic on `find-habit`

--- src/clj/astro/db/habits.clj
+++ src/clj/astro/db/habits.clj
@@ -2,50 +2,29 @@
   (:require [astro.db.core :refer [conn]]
             [datomic.api :as d]))
 
-(defn- find-all-habits
+(defn find-all-habits
   []
   (d/q '[:find (pull ?h [:db/id :habit/name {:habit/activities [:activity/date]}])
          :where [?h :habit/name]]
        (d/db conn)))
 
-(defn adapt-find-all-habits
-  [habits]
-  (for [[habit] habits]
-    {:id (:db/id habit)
-     :name (:habit/name habit)
-     :activities (map :activity/date (:habit/activities habit))}))
-
-(defn all-habits []
-  (adapt-find-all-habits (find-all-habits)))
-
-(defn find-habit-activities
-  [habit-name]
-  (d/q '[:find ?activity-date
-         :in $ ?habit-name
-         :where
-         [?eid :habit/name ?habit-name]
-         [?eid :habit/activities ?activity]
-         [?activity :activity/date ?activity-date]]
-       (d/db conn)
-       habit-name))
-
-(defn find-habit-id
-  [habit-name]
-  (ffirst (d/q '[:find ?eid
-                 :in $ ?habit-name
-                 :where [?eid :habit/name ?habit-name]]
+(defn find-habit
+  [name]
+  (ffirst (d/q '[:find (pull ?h [:db/id :habit/name {:habit/activities [:activity/date]}])
+                 :in $ ?name
+                 :where [?h :habit/name ?name]]
                (d/db conn)
-               habit-name)))
+               name)))
 
 (defn add-habit
   [habit-name]
   @(d/transact conn [{:db/id (d/tempid :db.part/user)
                       :habit/name habit-name}]))
 
-(defn register-activity
+(defn add-activity
   [habit-name activity-date]
   (let [activity-id (d/tempid :db.part/user)
-        habit-id (find-habit-id habit-name)]
+        habit-id (:db/id (find-habit habit-name))]
     @(d/transact conn [{:db/id activity-id
                         :activity/date activity-date}
                        {:db/id habit-id
--- src/clj/astro/routes/services/habits.clj
+++ src/clj/astro/routes/services/habits.clj
@@ -1,15 +1,25 @@
 (ns astro.routes.services.habits
   (:require [astro.db.habits :as habits]))
 
-(def habits (atom {}))
+(defn- serialize-habit
+  [habit]
+  {:id (:db/id habit)
+   :name (:habit/name habit)
+   :activities (map :activity/date (:habit/activities habit))})
+
+(defn- serialize-habits
+  [habits]
+  (for [[habit] habits]
+    (serialize-habit habit)))
 
 (defn all-habits
   [context args value]
-  (habits/all-habits))
+  (serialize-habits (habits/find-all-habits)))
 
 (defn get-habit
   [context {:keys [name]} value]
-  (first (filter #(= name (:name %)) @habits)))
+  (if-let [habit (habits/find-habit name)]
+    (serialize-habit habit)))
 
 (defn add-habit
   [context {:keys [name]} value]
@@ -17,4 +27,4 @@
 
 (defn add-activity
   [_ {:keys [name date]} _]
-  (habits/register-activity name date))
+  (habits/add-activity name date))
